68da541245af8b3ccc95a40d27af9856c1c792be
Jackelin <jackelinvasquezguzman@gmail.com>
2025-09-09 09:45:13 -0600

    Actividad 24

--- Actividad 24.py
+++ Actividad 24.py
@@ -1,1 +1,46 @@
 #Actividad 24
+
+def menu():
+    print("---MENÚ---")
+    print("1.Factorial de un numero")
+    print("2.Suma hasta n numero")
+    print("3.Calcular el numero Fibonacci")
+    print("4.Contar apariciones de letra")
+    print("5.Invertir cadena de texto.")
+    print("6.Calcular potencia de numero.")
+    print("7.Salir.")
+
+def factorial(numero):
+    if numero == 0 or numero==1:
+        return 1
+    else:
+        return numero * factorial(numero-1)
+
+def suma(numero):
+    if numero ==0 or numero ==1:
+        return 1
+    else:
+        numero= numero
+        return numero + suma(numero)
+
+def fibonacci(numero):
+    pass
+
+numero= int(input("INgrese un numero:"))
+print(suma(numero))
+
+"""""
+#programa principal
+menu()
+opcion= input("Ingrese una opcion:")
+match opcion:
+    case "1":
+        print("Factorial de numero")
+        numero=int(input("Ingrese un numero:"))
+        factorial(numero)
+    case "2":
+        print("Suma hasta n numero")
+        numero= int(input("Ingrese un numero:"))
+        suma(numero)
+        
+        """""
